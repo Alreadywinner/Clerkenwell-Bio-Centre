dcbe132b54a0a5d8d5c632e387e53e06cae3fbbb
Waseel Fawad <waseelfawad@gmail.com>
2024-04-26 01:35:35 +0500

    email template added for potential buyer update

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const cors = require("cors")({ origin: true });
 const dotenv = require("dotenv");
 const express = require("express");
+const EmailTemplate = require("./EmailTemplate");
 
 dotenv.config();
 // admin.initializeApp();
@@ -13,6 +14,39 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (request, response) => {
   return response.status(200).json({ msg: "hello World" });
+});
+
+app.post("/send-email", async (req, res) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.NODE_MAILER_EMAIL,
+        pass: process.env.NODE_MAILER_PASSWORD,
+      },
+    });
+    let info = await transporter.sendMail({
+      from: '"Clerkenwell Bio Centre" <nathandc90@gmail.com>', // sender address
+      to: "nathandc90@gmail.com", // list of receivers
+      subject: "Interested Buyer", // Subject line
+      text: "Greetings ! You have a new interested Buyer for tickets", // plain text body
+      html: EmailTemplate(
+        "John Wick",
+        "Baba Yaga",
+        "https://lionsgate.brightspotcdn.com/35/72/51b31d1045a2b4593871e545ebad/john-wick-movie-bg01-portrait.jpg",
+        "10$",
+        `https://lockersunlocked.com/payment`
+      ), // html body
+    });
+    res.status(200).json({
+      msg: `Message sent successfully ${info.messageId}`,
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 exports.app = functions.https.onRequest(app);
