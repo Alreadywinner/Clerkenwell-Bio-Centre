7126c5f8db5667a424739680709dbdfda07ccafb
Anas Nadeem <anasnadeem200406@gmail.com>
2024-04-26 00:42:20 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import CodeGenerate from "./CodeGenerate";
 import Questionnaire from "./Questionnaire";
+import Ticket from "./Ticket";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/code-generate" element={<CodeGenerate />} />
         <Route path="/questionnaire" element={<Questionnaire />} />
+        <Route path="/ticket" element={<Ticket />} />
       </Routes>
     </Router>
   );
